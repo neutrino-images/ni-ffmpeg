bc312114783e2263e606021fd4967bb1d38aa5f0
vanhofen <vanhofen@gmx.de>
2022-02-03 21:21:27 +0100

    - add force_dts_monotonicity switch

--- ffmpeg.h
+++ ffmpeg.h
@@ -361,6 +361,10 @@ typedef struct InputFile {
     int joined;                 /* the thread has been joined */
     int thread_queue_size;      /* maximum number of queued packets */
 #endif
+
+    // A value added to inbound timestamps to prevent them from going "backward" in cases such as HLS discontinuities
+    int64_t ff_timestamp_monotonicity_offset;
+
 } InputFile;
 
 enum forced_keyframes_const {
@@ -499,6 +503,9 @@ extern char *sdp_filename;
 extern float audio_drift_threshold;
 extern float dts_delta_threshold;
 extern float dts_error_threshold;
+
+extern int dts_monotonicity_threshold;
+extern int force_dts_monotonicity;
 
 extern int audio_volume;
 extern int audio_sync_method;
